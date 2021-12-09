f39f123c53f7144afb714fc0a8921820364372c7
Jack <jhuaihuah@unsa.edu.pe>
2021-12-08 23:27:14 -0500

    Refactorizacion: Form Template Method para eliminar codigo replicado del header

--- app/models/header.php
+++ app/models/header.php
@@ -1,10 +1,10 @@
 <?php
     class Header
     {
-        public $title;
-        public $css_path;
-        public $js_path;
-        public $icon_path;
+        private $title;
+        private $css_path;
+        private $js_path;
+        private $icon_path;
 
         public function __construct($title, $css_path, $js_path, $icon_path)
         {
@@ -12,5 +12,22 @@
             $this->css_path = $css_path;
             $this->js_path = $js_path;
             $this->icon_path = $icon_path;
-        }        
+        }
+
+        public function get_title()
+        {
+            return $this->title;
+        }
+        public function get_css_path()
+        {
+            return $this->css_path;
+        }
+        public function get_js_path()
+        {
+            return $this->js_path;
+        }
+        public function get_icon_path()
+        {
+            return $this->icon_path;
+        }
     }
