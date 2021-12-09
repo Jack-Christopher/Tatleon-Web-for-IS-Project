1a6583f7925a324a9e710befaa91a27b6d4fa557
Jack <jhuaihuah@unsa.edu.pe>
2021-12-08 21:56:43 -0500

    Refactorizacion: Preserve Whole Object para compatibilidad con header.php

--- controller/templates.php
+++ controller/templates.php
@@ -1,11 +1,12 @@
 <?php
-function print_header($title, $css_path, $js_path, $icon_path)
+// function with a Header object as parameter
+function print_header($header)
 {
     echo '<html lang="es">';
     echo '<head>';
     echo '<meta charset="UTF-8">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-    echo '<title>' . $title .'</title>';
+    echo '<title>' . $header->title .'</title>';
     // agregar archivo bootstrap
     echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
@@ -18,19 +19,19 @@ function print_header($title, $css_path, $js_path, $icon_path)
     echo '<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>';
     
     //  agregar archivo css
-    if ($css_path != '') 
+    if ($header->css_path != '') 
     {
-        echo '<link rel="stylesheet" href="' . $css_path .'">';
+        echo '<link rel="stylesheet" href="' . $header->css_path .'">';
     }
     // agregar icono
-    if ($icon_path != '') 
+    if ($header->icon_path != '') 
     {
-        echo '<link rel="icon" href="' . $icon_path . '">';
+        echo '<link rel="icon" href="' . $header->icon_path . '">';
     }
     // agregar archivo js
-    if ($js_path != '') 
+    if ($header->js_path != '') 
     {
-        echo '<script src="' . $js_path .'"></script>';
+        echo '<script src="' . $header->js_path .'"></script>';
     }
 
     echo '</head>';
