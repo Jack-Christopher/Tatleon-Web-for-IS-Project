ab52467b47647b9877b7b101652f634b179540f2
Jack <jhuaihuah@unsa.edu.pe>
2021-12-08 23:25:46 -0500

    Refactorizacion: Preserve Whole Object para compatibilidad con header.php

--- controller/templates.php
+++ controller/templates.php
@@ -6,7 +6,7 @@ function print_header($header)
     echo '<head>';
     echo '<meta charset="UTF-8">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-    echo '<title>' . $header->title .'</title>';
+    echo '<title>' . $header->get_title() .'</title>';
     // agregar archivo bootstrap
     echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
@@ -19,19 +19,19 @@ function print_header($header)
     echo '<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>';
     
     //  agregar archivo css
-    if ($header->css_path != '') 
+    if ($header->get_css_path() != '') 
     {
-        echo '<link rel="stylesheet" href="' . $header->css_path .'">';
+        echo '<link rel="stylesheet" href="' . $header->get_css_path() .'">';
     }
     // agregar icono
-    if ($header->icon_path != '') 
+    if ($header->get_icon_path() != '') 
     {
-        echo '<link rel="icon" href="' . $header->icon_path . '">';
+        echo '<link rel="icon" href="' . $header->get_icon_path() . '">';
     }
     // agregar archivo js
-    if ($header->js_path != '') 
+    if ($header->get_js_path() != '') 
     {
-        echo '<script src="' . $header->js_path .'"></script>';
+        echo '<script src="' . $header->get_js_path() .'"></script>';
     }
 
     echo '</head>';
